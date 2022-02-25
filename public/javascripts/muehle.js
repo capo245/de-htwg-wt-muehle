095691920a239d38316f80aec3022aebbcd3f466
capo245 <capo245@github.com>
2022-02-25 11:27:12 +0100

    Fixed Bootstrap and Added JS controller

--- public/javascripts/muehle.js
+++ public/javascripts/muehle.js
@@ -1,166 +1,18 @@
-let csrf = $('input[name="csrfToken"]').attr("name");
-let json;
-
-
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': csrf,
-        'Content-Type': 'application/json',
-        //'Accept': 'application/json'
-    }
-});
-
-function updateGrid() {
-    window.location.href = '/game'
-}
-
 function place(i) {
-    const field = $(`#field${i + 1}`);
-    if (field !== undefined) {
-        field.click(function () {
-            $.ajax({
-                method: "GET",
-                url: '/game/' + i,
-                dataType: "json",
-            });
-        })
-    } else {
-        throw Error('Selected Field is unavailable');
-    }
-}
-
-function move(i,j) {
-    const start = $(`#field${i + 1}`);
-    const destination = $(`#field${j + 1}`);
-    if ((start && destination) !== undefined && (start && destination) === true ) {
-        $(start).click(function () {
-            $(destination).click(function () {
-                $.ajax({
-                    method: "GET",
-                    url: '/move/' + i + '/' + j,
-                    dataType: "json",
-                });
-            });
-        })
-    }
-}
-
-function remove(i) {
-    const field = $(`#field${i + 1}`);
-    if (field.value !== '0') {
-        field.click(function () {
-            $.ajax({
-                method: "GET",
-                url: '/remove/' + i,
-                dataType: "json",
-            });
-        })
-    } else {
-        throw Error('Field does not contain removable stone');
-    }
-}
-
-function init_Buttons() {
-    $(`#start_game`).click(function () {
-        $.ajax({
-            method: "GET",
-            url: '/new',
-            dataType: "json",
-        });
-    });
-
-    $(`#undo`).click(function () {
-        $.ajax({
-            method: "GET",
-            url: '/undo',
-            dataType: "json",
-        });
-    });
-
-    $(`#redo`).click(function () {
-        $.ajax({
-            method: "GET",
-            url: '/redo',
-            dataType: "json",
-        });
-    });
-
-    $(`#save`).click(function () {
-        $.ajax({
-            method: "GET",
-            url: '/save',
-            dataType: "json",
-        });
-    });
-
-    $(`#load`).click(function () {
-        $.ajax({
-            method: "GET",
-            url: '/load',
-            dataType: "json",
-        });
-    })
-}
-
-//TODO: check JSON structure
-function loadJson() {
-    $.ajax({
-        method: "GET",
-        url: "/json",
-        dataType: "json",
-
-        success: function () {
-            for (let i = 1; i <= 24; i++) {
-                $(`field${i}`).clickEvent = place(i-1);
-            }
-            init_Buttons()
-        }
-    })
-}
-
-//example without using Ajax
-function init_Game() {
-    $('#start_game').click(function () {
+    $(`#field${i+1}`).click(async function () {
         const Http = new XMLHttpRequest();
-        const url = '/new';
+        const url='/game/' + i;
         Http.open("GET", url);
         Http.send();
-        window.location.href = '/game';
-    })
-}
-
-function connectWebSocket() {
-    const websocket = new WebSocket("ws://localhost:9000/websocket");
-    websocket.setTimeout;
-
-    websocket.onopen = function(event) {
-        console.log('Connected to Websocket');
-    };
-
-    websocket.onclose = function () {
-        console.log('Connection with Websocket Closed!');
-    };
-
-    websocket.onerror = function (error) {
-        console.log('Error in Websocket occurred: ' + error);
-    };
-
-    websocket.onmessage = function (e) {
-        if (typeof e.data === "string") {
-            json = JSON.parse(e.data);
-            updateGrid();
-            for (let i = 1; i <= 24; i++) {
-                $(`field${i}`).clickEvent = place(i-1);
-            }
-            init_Buttons()
-        }
-    };
+        window.location.href = url
+    });
 }
 
 $(document).ready(function () {
-    loadJson();
-    connectWebSocket();
-}) ;
+    for (i = 1; i <= 24; i++) {
+        document.getElementById(`field${i}`).onclick = place(i-1)
+    }
+})  ;
 
 $(document).ready(function () {
 
@@ -177,5 +29,3 @@ $(document).ready(function () {
     $('.animated-icon3').toggleClass('open');
   });
 });
-
-
